531d7fe0fd2aca68f259f792dfe930db747be7de
sunjianguo <1275596256@qq.com>
2023-03-07 11:40:45 +0800

    fix k8s deploy action

--- action/action_k8s_deploy.go
+++ action/action_k8s_deploy.go
@@ -68,7 +68,6 @@ func (k *K8sDeployAction) Hook() (*model.ActionResult, error) {
 		logger.Errorf("k8s containers format failed: %s", err.Error())
 		return nil, err
 	}
-	containers[0].Image = k.image
 	name := fmt.Sprintf("%s-%s", k.namespace, k.projectName)
 	_, err = utils.CreateDeployment(client, k.namespace, name, containers)
 	if err != nil {
