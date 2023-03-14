63942b3ca4a765cf7a5220b51a4196679e3c4a82
sunjianguo <1275596256@qq.com>
2023-03-14 10:35:38 +0800

    add k8s ingress query pod status

--- action/action_k8s_ingress.go
+++ action/action_k8s_ingress.go
@@ -3,12 +3,14 @@ package action
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/hamster-shared/aline-engine/logger"
 	"github.com/hamster-shared/aline-engine/model"
 	"github.com/hamster-shared/aline-engine/output"
 	"github.com/hamster-shared/aline-engine/utils"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type K8sIngressAction struct {
@@ -58,10 +60,23 @@ func (k *K8sIngressAction) Hook() (*model.ActionResult, error) {
 		return nil, err
 	}
 	serviceName := fmt.Sprintf("%s-%s", k.namespace, k.projectName)
-	_, err = utils.CreateIngress(client, k.namespace, serviceName, k.gateway, servicePorts)
+	service, err := utils.CreateIngress(client, k.namespace, serviceName, k.gateway, servicePorts)
 	if err != nil {
 		logger.Errorf("k8s create ingress  failed: %s", err.Error())
 		return nil, err
+	}
+	for {
+		pods, _ := client.CoreV1().Pods(k.namespace).List(context.Background(), metav1.ListOptions{
+			LabelSelector: fmt.Sprintf("app=%s", service.ObjectMeta.Name),
+		})
+		if len(pods.Items) > 0 {
+			if pods.Items[0].Status.Phase == corev1.PodRunning {
+				break
+			}
+			if pods.Items[0].Status.Phase == corev1.PodFailed {
+				return nil, errors.New("container deploy failed")
+			}
+		}
 	}
 	actionResult := &model.ActionResult{}
 	deployInfo := model.DeployInfo{
