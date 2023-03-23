783d86243b405b16e2175f73c0739ed212a53dc5
mohaijiang <mohaijiang110@163.com>
2023-03-23 22:07:09 +0800

    add docker env volume

--- model/step.go
+++ model/step.go
@@ -8,7 +8,7 @@ type Step struct {
 	Uses      string            `yaml:"uses,omitempty" json:"uses"`
 	With      map[string]string `yaml:"with,omitempty" json:"with"`
 	RunsOn    string            `yaml:"runs-on,omitempty" json:"runsOn"`
-	Volumes   []string          `yaml:"volumes" json:"volumes"`
+	Volumes   []string          `yaml:"volumes,omitempty" json:"volumes"`
 	Run       string            `yaml:"run,omitempty" json:"run"`
 	Status    Status            `yaml:"status,omitempty" json:"status"`
 	StartTime time.Time         `yaml:"startTime,omitempty" json:"startTime"`
