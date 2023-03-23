3aacadac37bd2aa78d2865842aa4475a7a6a6fde
sunjianguo <1275596256@qq.com>
2023-03-23 17:02:22 +0800

    change image build action

--- action/action_image_build.go
+++ action/action_image_build.go
@@ -14,7 +14,6 @@ import (
 
 type ImageBuildAction struct {
 	imageName string
-	runBuild  string
 	output    *output.Output
 	ctx       context.Context
 }
@@ -22,7 +21,6 @@ type ImageBuildAction struct {
 func NewImageBuildAction(step model.Step, ctx context.Context, output *output.Output) *ImageBuildAction {
 	return &ImageBuildAction{
 		imageName: step.With["image_name"],
-		runBuild:  step.With["run_build"],
 		ctx:       ctx,
 		output:    output,
 	}
@@ -33,8 +31,6 @@ func (i *ImageBuildAction) Pre() error {
 	params := stack["parameter"].(map[string]string)
 	i.imageName = utils.ReplaceWithParam(i.imageName, params)
 	logger.Debugf("k8s build image is : %s", i.imageName)
-	i.runBuild = utils.ReplaceWithParam(i.runBuild, params)
-	logger.Debugf("run build is: %s", i.runBuild)
 	return nil
 }
 
@@ -43,18 +39,6 @@ func (i *ImageBuildAction) Hook() (*model.ActionResult, error) {
 	workdir, ok := stack["workdir"].(string)
 	if !ok {
 		return nil, errors.New("get workdir error")
-	}
-	if i.runBuild == "true" {
-		installCmd := []string{"npm", "install"}
-		_, err := i.ExecuteCommand(installCmd, workdir)
-		if err != nil {
-			return nil, errors.New("npm install failed")
-		}
-		codeBuildCmd := []string{"npm", "run", "build"}
-		_, err = i.ExecuteCommand(codeBuildCmd, workdir)
-		if err != nil {
-			return nil, errors.New("npm run build failed")
-		}
 	}
 	buildCommands := []string{"docker", "build", "-t", i.imageName, "."}
 	_, err := i.ExecuteCommand(buildCommands, workdir)
