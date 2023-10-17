fa2adc8ef5db9ff6b4f226ea06a7e34a9597fcd2
abing <410174833@qq.com>
2023-10-17 11:07:06 +0800

    git log

--- action/action_git.go
+++ action/action_git.go
@@ -146,7 +146,7 @@ func (a *GitAction) Hook() (*model2.ActionResult, error) {
 	}
 
 	//dateCommand := "git --no-pager log --pretty=format:“%cd” --date=format:'%b %e %Y' " + commitId
-	dateCommand := []string{"git", "--no-pager", "log", "--pretty=format:“%cd”", "--date=format:'%b %e %Y'", "-1", commitId}
+	dateCommand := []string{"git", "--no-pager", "log", "--pretty=format:'%cd'", "--date=format:'%b %e %Y'", "-1", commitId}
 	commitDate, err := a.ExecuteCommandDirect(dateCommand)
 	if err != nil {
 		return nil, err
